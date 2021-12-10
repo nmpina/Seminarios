020df2fc2a190c37d0236cdb3d1d860f5f8ef488
nelson <nelson@snigelweb.com>
2021-12-10 21:20:58 +0000

    Add function to create table Oradores. Allow title field to be null

--- app/src/main/java/pt/ipg/seminarios/Orador.kt
+++ app/src/main/java/pt/ipg/seminarios/Orador.kt
@@ -1,5 +1,5 @@
 package pt.ipg.seminarios
 
-data class Orador (var nome: String, var tipo : String, var  id: Long = -1) {
+data class Orador (var nome: String, var tipo : String?, var  id: Long = -1) {
 
 }
--- app/src/main/java/pt/ipg/seminarios/TabelaOradores.kt
+++ app/src/main/java/pt/ipg/seminarios/TabelaOradores.kt
@@ -2,5 +2,15 @@ package pt.ipg.seminarios
 
 import android.database.sqlite.SQLiteDatabase
 
-class TabelaOradores(var db: SQLiteDatabase) {
+class TabelaOradores(db: SQLiteDatabase) {
+    private val db = db
+
+    public fun cria() {
+        db.execSQL(
+            "CREATE TABLE oradores (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "nome TEXT,NOT NULL" +
+                    "titulo TEXT" + ")"
+        )
+    }
 }
