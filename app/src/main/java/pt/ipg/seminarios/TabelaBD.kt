bf83ba593ece59d10627e4a2d12b3e47a69adf4e
nelson <nelson@snigelweb.com>
2021-12-17 16:32:02 +0000

    Create CRUD functions for TabelaOradores. Create base class TabelaBD

---
+++ app/src/main/java/pt/ipg/seminarios/TabelaBD.kt
@@ -0,0 +1,25 @@
+package pt.ipg.seminarios
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+
+open class TabelaBD(db: SQLiteDatabase, nomeTabela: String) {
+    protected val  db=db;
+    protected  val nomeTabela = nomeTabela
+
+    fun insert (values: ContentValues) {
+        db.insert(nomeTabela, null, values)
+    }
+
+    fun  update (id: Long, values: ContentValues) {
+        db.update(nomeTabela, values, "${TabelaOradores.CAMPO_ID}=?", arrayOf("$id"))
+    }
+
+    fun delete (id: Long) {
+        db.delete(nomeTabela,"${TabelaOradores.CAMPO_ID}=?", arrayOf("$id"))
+    }
+
+    fun query(columns: Array<String>, selection: String, selectionArgs: Array<String>, groupBy: String, having: String, orderBy: String ) {
+        db.query(nomeTabela, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+}
--- app/src/main/java/pt/ipg/seminarios/TabelaOradores.kt
+++ app/src/main/java/pt/ipg/seminarios/TabelaOradores.kt
@@ -1,16 +1,19 @@
 package pt.ipg.seminarios
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
-class TabelaOradores(db: SQLiteDatabase) {
-    private val db = db
+class TabelaOradores(db: SQLiteDatabase) : TabelaBD (db, NOME_TABELA) {
 
-    public fun cria() {
-        db.execSQL(
-            "CREATE TABLE oradores (" +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    "nome TEXT,NOT NULL" +
-                    "titulo TEXT" + ")"
-        )
+    fun cria() {
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CAMPO_ID INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL $CAMPO_TITULO TEXT)")
+    }
+
+    companion object {
+        const val NOME_TABELA = "oradores"
+        const val CAMPO_ID = BaseColumns._ID
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_TITULO = "titulo"
     }
 }
