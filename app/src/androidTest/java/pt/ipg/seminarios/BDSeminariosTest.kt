b0d2bc064afc4ba0f88550286a1ab12680792edf
nelson <nelson@snigelweb.com>
2021-12-17 18:29:50 +0000

    Create test to check if it's possible to delete DB (incomplete)

--- app/src/androidTest/java/pt/ipg/seminarios/BDSeminariosTest.kt
+++ app/src/androidTest/java/pt/ipg/seminarios/BDSeminariosTest.kt
@@ -1,5 +1,6 @@
 package pt.ipg.seminarios
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -16,6 +17,21 @@ import org.junit.Before
  */
 @RunWith(AndroidJUnit4::class)
 class BDSeminariosTest {
+
+    private fun getContext() = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private fun getWritableDatabase(): SQLiteDatabase {
+        val bdOpenHelper = BaseDadosSeminariosOpenHelper(getContext())
+        return bdOpenHelper.writableDatabase
+    }
+
+    private fun insereOrador( tabelaOradores: TabelaOradores, orador: Orador ) : Long {
+        val id = tabelaOradores.insert(orador.toContentValues())
+
+        assertNotEquals(-1, id)
+        return id
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
@@ -28,12 +44,27 @@ class BDSeminariosTest {
         getContext().deleteDatabase(BaseDadosSeminariosOpenHelper.NOME_BASE_DADOS)
     }
 
-    private fun getContext() = InstrumentationRegistry.getInstrumentation().targetContext
-
     @Test
     fun consegueCriarAbrirBaseDados() {
         val bdOpenHelper= BaseDadosSeminariosOpenHelper(getContext())
         val bd = bdOpenHelper.readableDatabase
         assert(bd.isOpen)
+    }
+
+    @Test
+    fun consegueInserirOrador() {
+        val bd = getWritableDatabase()
+
+        insereOrador(TabelaOradores(bd), Orador("Fábio Marques", "Sr."))
+        bd.close()
+    }
+
+
+
+    @Test
+    fun consegueEliminarOrador() {
+        val bd = getWritableDatabase()
+        insereOrador(TabelaOradores(bd), Orador("Anonimo", ""))
+        bd.close()
     }
 }
